246bd050c57624f2e7851ab5f005da7a948c4cc1
V. Hill <67915924+V-Hill@users.noreply.github.com>
2020-12-25 11:56:10 +0000

    Added in LSTM model

--- src/rnn_jet_data.py
+++ src/rnn_jet_data.py
@@ -5,6 +5,7 @@ This file contains the code for preparing the jet data for an RNN neural network
 
 # Code from other files in the repo
 from utilities.data_loader import  DataLoader
+import models.recurrent_models as recurrent_models
 from utilities.data_preprocessing import DataProcessing
 from utilities.data_preprocessing import LabelMaker
 from utilities.data_preprocessing import WeightMaker
@@ -17,7 +18,8 @@ import time
 from sklearn.model_selection import train_test_split
 
 ROOT = "C:\\{Directory containing data}\\ml_postproc\\"
-data_to_collect = ['ttH125_part1-1', 
+data_to_collect = ['ttH125_part1-1',
+                   'ttH125_part1-2', 
                    'TTTo2L2Nu', 
                    'TTToHadronic', 
                    'TTToSemiLeptonic']
@@ -90,3 +92,12 @@ rt = make_ragged_tensor(df)
 print(f"Shape: {rt.shape}")
 print(f"Number of partitioned dimensions: {rt.ragged_rank}")
 print(f"Flat values shape: {rt.flat_values.shape}")
+
+# ------------------------------ Model training -------------------------------
+
+model = recurrent_models.base()
+history = model.fit(rt, 
+                    event_labels, 
+                    sample_weight=sample_weight, 
+                    epochs=4, 
+                    verbose=2)
