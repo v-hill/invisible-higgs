f54ce53e4227f6f505bf0a1e4c2a8bc7b51713d8
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-01-28 10:35:18 +0000

    Update nn_event_data.py
    Removed the data prepressing code in replace of importing the pre-processed data generated by 'preprocess_data.py'

--- src/nn_event_data.py
+++ src/nn_event_data.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 This file contains the run code for a simple feedforward neural network to 
 classify different event types.
@@ -5,13 +6,9 @@ classify different event types.
 
 # Code from other files in the repo
 import models.sequential_models as sequential_models
-from utilities.data_loader import  DataLoader
-from utilities.data_preprocessing import DataProcessing
-from utilities.data_preprocessing import LabelMaker
-from utilities.data_preprocessing import WeightMaker
+import utilities.plotlib as plotlib
 
 # Python libraries
-import pandas as pd
 import numpy as np
 import time
 from sklearn.model_selection import train_test_split
@@ -24,40 +21,14 @@ data_to_collect = ['ttH125_part1-1',
 
 # -------------------------------- Data setup --------------------------------
 
-# Load in data
-loader = DataLoader(ROOT)
-loader.find_files()
-loader.collect_data(data_to_collect)
-data = DataProcessing(loader)
-
-cols_to_ignore = ['entry', 'weight_nominal', 'hashed_filename']
-cols_events = data.get_event_columns(cols_to_ignore)
-# cols_jets = data.get_jet_columns()
-
-data.set_nan_to_zero('DiJet_mass')
-# data.remove_nan('DiJet_mass')
-
-signal_list = ['ttH125']
-data.label_signal_noise(signal_list)
-#event_labels = LabelMaker.onehot_encoding(data.return_dataset_labels())
-event_labels = LabelMaker.label_encoding(data.return_dataset_labels())
-data.set_dataset_labels(event_labels)
-
-# class_weight = WeightMaker.event_class_weights(data)
-sample_weight = WeightMaker.weight_nominal_sample_weights(data)
-
-# Select only the filtered columns from the data
-data.filter_data(cols_events)
-
-cols_to_log = ['HT', 'MHT_pt', 'MetNoLep_pt']
-data.nat_log_columns(cols_to_log)
-
-min_max_scale_range = (0, 1)
-data.normalise_columns(min_max_scale_range)
+event_data = np.load('preprocessed_event_data')
+event_labels = np.load('preprocessed_event_labels')
+sample_weight = np.load('preprocessed_sample_weights')
 
 test_fraction = 0.2
 data_train, data_test, labels_train, labels_test, sw_train, sw_test  = \
-    train_test_split(data.data, event_labels, sample_weight, test_size=test_fraction)
+    train_test_split(event_data.data, event_labels, 
+                     sample_weight, test_size=test_fraction)
 
 # ------------------------------ Model training -------------------------------
 
@@ -81,7 +52,7 @@ fig1 = plotlib.training_history_plot(history, 'Event neural network model accura
 # Make confsuion matrix
 labels_pred = model.predict(data_test)
 labels_pred = np.argmax(labels_pred, axis=1)
-cm = confusion_matrix(labels_test, labels_pred)
+cm = plotlib.confusion_matrix(labels_test, labels_pred)
 class_names = ['signal', 'background']
 title = 'Confusion matrix'
 
