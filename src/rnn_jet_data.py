7094f4aa755f1be1abdf142481ec2c6e88159411
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-01-28 00:36:54 +0000

    Removed make_ragged_tensor()
    Function is now located in the utilities.data_preprocessing script

--- src/rnn_jet_data.py
+++ src/rnn_jet_data.py
@@ -8,7 +8,7 @@ import models.recurrent_models as recurrent_models
 from utilities.data_preprocessing import DataProcessing
 from utilities.data_preprocessing import LabelMaker
 from utilities.data_preprocessing import WeightMaker
-# from utilities.data_preprocessing import split_data
+from utilities.data_preprocessing import make_ragged_tensor
 
 # Python libraries
 import pandas as pd
@@ -45,45 +45,6 @@ sample_weight = WeightMaker.weight_nominal_sample_weights(data)
 data.filter_data(cols_jets)
 
 # TODO: Normalise the jet data on a per variable basis
-
-# ---------------------------- Making RaggedTensor ----------------------------
-
-# See: 'Uniform inner dimensions' in tensorflow documentation:
-#       https://www.tensorflow.org/guide/ragged_tensor
-
-def make_ragged_tensor(input_data):
-    """
-    Turns a pandas dataframe of the jet data into a tensorflow ragged tensor.
-
-    Parameters
-    ----------
-    input_data : pandas.DataFrame
-        Dataframe containing the jet data.
-
-    Returns
-    -------
-    rt : tensorflow.RaggedTensor
-        Ragged tensor with 3 dimensions:
-        TensorShape([{Number of events}, {Number of jets in the event}, 6])
-        Each jet is specified by 6 values, with a variable number of jets per
-        event. This second dimension denoting the number of jets is a ragged
-        dimension.
-    """
-    data_list = input_data.values.tolist()
-    
-    row_current = 0
-    row_splits = [0]
-    rt_list_new = []
-    for idx, event in enumerate(data_list):
-        rt = np.stack(event, axis=0).T.tolist()
-        row_current += len(rt)
-        rt_list_new += rt
-        row_splits.append(row_current)
-    
-    rt = tf.RaggedTensor.from_row_splits(
-        values=rt_list_new,
-        row_splits=row_splits)
-    return rt
 
 df = data.data
 rt = make_ragged_tensor(df)
