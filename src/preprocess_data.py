98a66385e3c3aad1da6f211286b125a8037a1d38
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-01-28 00:51:19 +0000

    Update preprocess_data.py

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -8,6 +8,7 @@ from utilities.data_loader import  DataLoader
 from utilities.data_preprocessing import DataProcessing
 from utilities.data_preprocessing import LabelMaker
 from utilities.data_preprocessing import WeightMaker
+from utilities.data_preprocessing import make_ragged_tensor
 
 # Python libraries
 from sklearn.model_selection import train_test_split
@@ -28,7 +29,7 @@ data = DataProcessing(loader)
 
 cols_to_ignore = ['entry', 'weight_nominal', 'hashed_filename']
 cols_events = data.get_event_columns(cols_to_ignore)
-# cols_jets = data.get_jet_columns()
+cols_jets = data.get_jet_columns()
 
 data.set_nan_to_zero('DiJet_mass')
 # data.remove_nan('DiJet_mass')
@@ -42,16 +43,25 @@ data.set_dataset_labels(event_labels)
 # class_weight = WeightMaker.event_class_weights(data)
 sample_weight = WeightMaker.weight_nominal_sample_weights(data)
 
-# Select only the filtered columns from the data
-data.filter_data(cols_events)
+# Make seperate copies of the dataset
+event_data = copy.deepcopy(data)
+jet_data = copy.deepcopy(data)
+
+# Select only the event columns from the data
+event_data.filter_data(cols_events)
+
+# Select only the jet columns from the data
+jet_data.filter_data(cols_jets)
+
 
 cols_to_log = ['HT', 'MHT_pt', 'MetNoLep_pt']
-data.nat_log_columns(cols_to_log)
+event_data.nat_log_columns(cols_to_log)
 
 min_max_scale_range = (0, 1)
-data.normalise_columns(min_max_scale_range)
+event_data.normalise_columns(min_max_scale_range)
 
-test_fraction = 0.2
-data_train, data_test, labels_train, labels_test, sw_train, sw_test  = \
-    train_test_split(data.data, event_labels, sample_weight, test_size=test_fraction)
+df_jet_data = jet_data.data
+rt_jet_data = make_ragged_tensor(df_jet_data)
     
+# -------------------------------- Data saving --------------------------------
+
