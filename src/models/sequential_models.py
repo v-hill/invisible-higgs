17ec7734a88ba54e8c97370850f36948b6ee3d1e
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-02-17 23:30:33 +0000

    Update sequential_models.py

--- src/models/sequential_models.py
+++ src/models/sequential_models.py
@@ -17,10 +17,40 @@ else:
 
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
 
 # -----------------------------------------------------------------------------  
 
 def base(layer1, layer2, input_shape=12):
+    """
+    This functoin creates a dense/fully connected neuarl network with 2 hidden
+    layers.
+    Parameters
+    ----------
+    layer1 : int
+        Number of neurons in the first layer
+    layer2 : int
+        Number of neurons in the second layer
+    input_shape : int, optional
+        The default is 12.
+    Returns
+    -------
+    model : keras.Sequential
+    """
+    # Define Sequential model with 2 hidden layers
+    model = keras.Sequential()
+    model.add(keras.Input(shape=(input_shape,)))
+    model.add(layers.Dense(layer1, activation="relu"))
+    model.add(layers.Dense(layer2, activation="relu"))
+    model.add(layers.Dense(2))
+    
+    # Compile model
+    model.compile(optimizer='adam',
+              loss=SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+    return model
+
+def base2(layer1, layer2, input_shape=12):
     """
     This functoin creates a dense/fully connected neuarl network with 2 hidden
     layers.
