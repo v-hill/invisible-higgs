2c828a63c0ac1fc00a66e1e01c5344b8da954d47
khalil pierre <kp16050@my.bristol.ac.uk>
2021-02-25 03:18:39 +0000

    Made histograms of the event data when the sample weight is applied. Results can be found in images/event_hist and src code can be found in src/hist_plt.py.

---
+++ src/hist_plt.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+This aim of this script is to produce histograms of the different event 
+variables.
+"""
+
+# Code from other files in the repo
+from utilities.data_loader import  DataLoader
+from utilities.data_preprocessing import DataProcessing
+from utilities.data_preprocessing import LabelMaker
+from utilities.data_preprocessing import WeightMaker
+from utilities.data_preprocessing import normalise_jet_columns
+
+# Python libraries
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+
+ROOT = "C:\\Users\\user\\Documents\\Fifth Year\\ml_postproc"
+data_to_collect = ['ttH125_part1-1', 
+                   'TTTo2L2Nu', 
+                   'TTToHadronic', 
+                   'TTToSemiLeptonic']
+
+# -------------------------------- Data setup --------------------------------
+
+# Load in data
+loader = DataLoader(ROOT)
+loader.find_files()
+loader.collect_data(data_to_collect)
+data = DataProcessing(loader)
+data.data['sample_weight'] = WeightMaker.weight_nominal_sample_weights(data)
+
+cols_to_ignore1 = ['entry', 'weight_nominal', 'hashed_filename']
+cols_to_ignore2 = ['cleanJetMask']
+cols_events = data.get_event_columns(cols_to_ignore1)
+cols_jets = data.get_jet_columns(cols_to_ignore2)
+
+signal_df = data.data[data.data.dataset == 'ttH125']
+background_df1 = data.data[data.data.dataset == 'TTTo2L2Nu']
+background_df2 = data.data[data.data.dataset == 'TTToHadronic']
+background_df3 = data.data[data.data.dataset == 'TTToSemiLeptonic']
+
+sample_weight0 = signal_df.sample_weight.values
+sample_weight1 = background_df1.sample_weight.values
+sample_weight2 = background_df2.sample_weight.values
+sample_weight3 = background_df3.sample_weight.values
+
+
+for col in cols_events:
+
+    fig1,axes = plt.subplots(ncols=2)
+    ax1,ax2 = axes.flatten()
+
+    ax1.hist(signal_df[col], bins=100, alpha=0.5, label='ttH125')
+    ax1.hist(background_df1[col], bins=100, alpha=0.5, label='TTTo2L2Nu')
+    ax1.hist(background_df2[col], bins=100, alpha=0.5, label='TTToHadronic')
+    ax1.hist(background_df3[col], bins=100, alpha=0.5, label='TTToSemiLeptonic')
+    ax1.set_xlabel(col)
+    ax1.legend()   
+
+    ax2.hist(signal_df[col], bins=100, weights=sample_weight0, alpha=0.5, label='ttH125')
+    ax2.hist(background_df1[col], bins=100, weights=sample_weight1, alpha=0.5, label='TTTo2L2Nu')
+    ax2.hist(background_df2[col], bins=100, weights=sample_weight2, alpha=0.5, label='TTToHadronic')
+    ax2.hist(background_df3[col], bins=100, weights=sample_weight3, alpha=0.5, label='TTToSemiLeptonic')
+    ax2.set_xlabel(col)
+    ax2.legend()
+    
+
+
+
+
+
+
+
+
+
+
+
+
