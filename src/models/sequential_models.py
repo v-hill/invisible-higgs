eee2d63251caf6b6d088434e10f0ee99dbf7f15e
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-03-10 12:35:54 +0000

    Update sequential_models.py

--- src/models/sequential_models.py
+++ src/models/sequential_models.py
@@ -79,7 +79,42 @@ def base2(layer1, layer2, input_shape=12):
                            kernel_initializer='random_normal'))
     
     # Compile model
-    model.compile(optimizer='adam',
+    opt = keras.optimizers.Adam(learning_rate=0.002)
+    model.compile(optimizer=opt,
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+    return model
+
+def base3(layer1, layer2, input_shape=12):
+    """
+    This functoin creates a dense/fully connected neuarl network with 2 hidden
+    layers.
+
+    Parameters
+    ----------
+    layer1 : int
+        Number of neurons in the first layer
+    layer2 : int
+        Number of neurons in the second layer
+    input_shape : int, optional
+        The default is 12.
+
+    Returns
+    -------
+    model : keras.Sequential
+    """
+    # Define Sequential model with 2 hidden layers
+    model = keras.Sequential()
+    model.add(keras.Input(shape=(input_shape,)))
+    model.add(layers.Dense(layer1, activation='relu', 
+                           kernel_initializer='random_normal'))
+    model.add(layers.Dense(layer2, activation='relu', 
+                           kernel_initializer='random_normal'))
+    model.add(layers.Dense(1, activation='sigmoid', 
+                           kernel_initializer='random_normal'))
+    
+    # Compile model
+    model.compile(optimizer='Adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
     return model
