c0983fdd6a230aac7ea2fa78f7573b8af9d51c62
khalil pierre <kp16050@my.bristol.ac.uk>
2021-03-12 19:15:13 +0000

    Created and trained a network that is capable of multilabel classification results can be seen in images

--- src/models/sequential_models.py
+++ src/models/sequential_models.py
@@ -23,7 +23,7 @@ from tensorflow.keras.losses import SparseCategoricalCrossentropy
 
 def base(layer1, layer2, input_shape=11):
     """
-    This function creates a dense/fully connected neuarl network with 2 hidden
+    This functoin creates a dense/fully connected neuarl network with 2 hidden
     layers.
     Parameters
     ----------
@@ -50,10 +50,10 @@ def base(layer1, layer2, input_shape=11):
               metrics=['accuracy'])
     return model
 
-def base2(layer1, layer2, input_shape=11, learning_rate=0.01):
+def base2(layer1, layer2, input_shape=11):
     """
-    base + custom learning rate for the Adam optimiser
-         + 'binary_crossentropy' loss function
+    This functoin creates a dense/fully connected neuarl network with 2 hidden
+    layers.
 
     Parameters
     ----------
@@ -79,7 +79,7 @@ def base2(layer1, layer2, input_shape=11, learning_rate=0.01):
                            kernel_initializer='random_normal'))
     
     # Compile model
-    opt = keras.optimizers.Adam(learning_rate=learning_rate)
+    opt = keras.optimizers.Adam(learning_rate=0.002)
     model.compile(optimizer=opt,
               loss='binary_crossentropy',
               metrics=['accuracy'])
@@ -87,7 +87,8 @@ def base2(layer1, layer2, input_shape=11, learning_rate=0.01):
 
 def base3(layer1, layer2, input_shape=11):
     """
-    base2 with standard learning rate
+    This functoin creates a dense/fully connected neuarl network with 2 hidden
+    layers.
 
     Parameters
     ----------
@@ -118,9 +119,9 @@ def base3(layer1, layer2, input_shape=11):
               metrics=['accuracy'])
     return model
 
-def base_with_dropout(layer1, layer2, input_shape=11, dropout=0.2):
+def base_with_dropout(layer1, layer2, input_shape=11):
     """
-    Functionally identical to base3 model, but with the addition of two 
+    Functionally identical to base model, but with the addition of two 
     dropout layers after each hidden layer.
     Returns
     -------
@@ -129,17 +130,59 @@ def base_with_dropout(layer1, layer2, input_shape=11, dropout=0.2):
     # Define Sequential model with 2 hidden layers
     model = keras.Sequential()
     model.add(keras.Input(shape=(input_shape,)))
-    model.add(layers.Dense(layer1, activation='relu', 
-                           kernel_initializer='random_normal'))
-    model.add(layers.Dropout(dropout))
-    model.add(layers.Dense(layer2, activation='relu', 
-                           kernel_initializer='random_normal'))
-    model.add(layers.Dropout(dropout))
-    model.add(layers.Dense(1, activation='sigmoid', 
-                           kernel_initializer='random_normal'))
+    model.add(layers.Dense(layer1, activation="relu"))
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(layer2, activation="relu"))
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Dense(1))
     
     # Compile model
     model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
     return model
+
+def multi_class_base(layer1, layer2, input_shape=11, output_shape=4):
+    '''
+    This function creates a dense/fully connected neural network with 2 
+    hidden layers for multi label classification.
+
+    Parameters
+    ----------
+    layer1 : int
+        Number of neurons in layer 1 .
+    layer2 : int
+        Number of neurons in layer 2 .
+    input_shape : int, optional
+        The input shape of the data. The default is 11.
+
+    Returns
+    -------
+    model : keras.Sequential
+    '''
+    
+    model = keras.Sequential()
+    model.add(keras.Input(shape=(input_shape,)))
+    model.add(layers.Dense(layer1, activation='relu', 
+                           kernel_initializer='random_normal'))
+    model.add(layers.Dense(layer2, activation='relu', 
+                           kernel_initializer='random_normal'))
+    model.add(layers.Dense(output_shape, activation='softmax', 
+                           kernel_initializer='random_normal'))
+    
+    # Compile model
+    model.compile(optimizer='Adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+    return model
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/multi_classification_nn.py
@@ -0,0 +1,87 @@
+"""
+This file contains the training script for a multilabel feedforward neural
+network
+"""
+
+# ---------------------------------- Imports ----------------------------------
+
+# Code from other files in the repo
+import models.sequential_models as sequential_models
+import utilities.plotlib as plotlib
+
+# Python libraries
+import pickle
+import numpy as np
+import pandas as pd
+import time
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+# -------------------------------- Data setup --------------------------------
+
+SAVE_FOLDER = 'data_multi_classifier'
+DIR = SAVE_FOLDER + '\\'
+
+#Load files
+event_data = np.load(DIR+'preprocessed_event_data.npy', allow_pickle=True)
+sample_weight = np.load(DIR+'preprocessed_sample_weights.npy', allow_pickle=True)
+encoding_dict = pickle.load(open(DIR+'encoding_dict.pickle', 'rb'))
+event_labels = pd.read_hdf(DIR+'preprocessed_event_labels.hdf')
+event_labels = event_labels.values
+
+test_fraction = 0.2
+data_train, data_test, labels_train, labels_test, sw_train, sw_test  = \
+    train_test_split(event_data, event_labels, 
+                     sample_weight, test_size=test_fraction)
+
+# Take a sample of the data to speed up training
+sample_num = -1
+data_train = data_train[:sample_num]
+data_test = data_test[:sample_num]
+labels_train = labels_train[:sample_num]
+labels_test = labels_test[:sample_num]
+sw_train = sw_train[:sample_num]
+sw_test = sw_test[:sample_num]
+
+# ------------------------------ Model training -------------------------------
+
+model = sequential_models.multi_class_base(64, 8, input_shape=11)
+
+print("Fitting sequential model on event training data...")
+START = time.time()
+history = model.fit(data_train, labels_train, batch_size = 64,
+                    validation_data=(data_test, labels_test), 
+                    sample_weight=sw_train, epochs=32, verbose=2)
+print(f"    Elapsed training time: {time.time()-START:0.2f}s")
+
+test_loss, test_acc = model.evaluate(data_test, labels_test, verbose=2)
+print(f"    Test accuracy: {test_acc:0.5f}")
+
+# --------------------------------- Plotting ----------------------------------
+
+# Plot training history
+fig1 = plotlib.training_history_plot(history, 'Event neural network model accuracy')
+
+
+# Get model predictions
+labels_pred = model.predict(data_test)
+labels_pred = np.argmax(labels_pred, axis=1)
+labels_test = np.argmax(labels_test, axis=1)
+
+# Create confusion matrix
+cm =  confusion_matrix(labels_test, labels_pred)
+class_names = list(encoding_dict.keys())
+title = 'Confusion matrix'
+
+# Plot confusion matrix
+fig2 = plotlib.confusion_matrix(cm, class_names, title)
+
+
+
+
+
+
+
+
+
