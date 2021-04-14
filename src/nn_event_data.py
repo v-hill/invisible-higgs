e66c1dd0f11e56e7240177892d6dc961ffb715c8
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-04-14 18:50:21 +0100

    Added raw code for significance plot
    TODO: Update plotlib for significance plot

--- src/nn_event_data.py
+++ src/nn_event_data.py
@@ -17,6 +17,7 @@ import time
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+from math import sqrt
 
 # -------------------------------- Data setup --------------------------------
 
@@ -67,19 +68,15 @@ print(f"    Test accuracy: {test_acc:0.5f}")
 fig1 = plotlib.training_history_plot(history, 'Event neural network model accuracy')
 
 
-# Get model predictions
+# Get model predictions and convert predictions into binary values
 labels_pred = model.predict(data_test)
-
-# Convert predictions into binary values
 cutoff_threshold = 0.5
 labels_pred_binary = np.where(labels_pred > cutoff_threshold, 1, 0)
 
 # Make confsuion matrix
 cm = confusion_matrix(labels_test, labels_pred_binary)
-class_names = ['signal', 'background']
-title = f'Confusion matrix for threshold of {cutoff_threshold}'
-
-# Plot confusion matrix
+class_names = ['ttH (signal)', 'tt¯ (background)']
+title = f'Confusion matrix for discriminator threshold of {cutoff_threshold}'
 fig2 = plotlib.confusion_matrix(cm, class_names, title)
 
 
@@ -93,3 +90,59 @@ labels_pred_signal = labels_pred[np.array(labels_test, dtype=bool)]
 labels_pred_background = labels_pred[np.invert(np.array(labels_test, dtype=bool))]
 title = "Distribution of discriminator values for the event-nn"
 fig4 = plotlib.plot_discriminator_vals(labels_pred_signal, labels_pred_background, title)
+
+#%%
+
+"""
+Raw code for significance plot.
+"""
+
+# Get model predictions
+labels_pred = model.predict(event_data)
+xs_weight = xs_weight.reshape((-1, 1))
+
+thresholds = np.linspace(0, 1, 50)
+bin_centres_sig = []
+bin_vals_sig = []
+bin_centres_back = []
+bin_vals_back = []
+
+dataset = pd.DataFrame(data=labels_pred, columns=['labels_pred'])
+dataset['xs_weight'] = xs_weight*140000
+dataset['event_labels'] = event_labels
+
+for i in range(len(thresholds)-1):
+    df_selection = dataset[dataset['labels_pred'].between(thresholds[i], 1)]
+    df_sig = df_selection[df_selection['event_labels']==1]
+    df_back = df_selection[df_selection['event_labels']==0]
+    sum_xs_weight_sig = df_sig['xs_weight'].sum()
+    sum_xs_weight_back = df_back['xs_weight'].sum()
+    
+    # print(f'{thresholds[i]:0.2f}, {thresholds[i+1]:0.2f}, {len(df_selection)}, {sum_xs_weight:0.2f}')
+    
+    bin_centres_sig.append(thresholds[i])
+    bin_vals_sig.append(sum_xs_weight_sig)
+    bin_centres_back.append(thresholds[i])
+    bin_vals_back.append(sum_xs_weight_back)
+
+z_vals = []
+z_vals2 = []
+print('threshold, signal, background, s/sqrt(b)')
+for i in range(len(bin_vals_sig)):
+    s = bin_vals_sig[i]
+    b = bin_vals_back[i]
+    
+    z = sqrt(2*((s+b)*np.log(1+(s/b))-s)) # Calculate significance 
+    z_vals.append(z)
+    z_vals2.append(s/(sqrt(b)))
+
+z_vals = np.asarray(z_vals)
+
+fig = plt.figure(figsize=(6, 4), dpi=200)
+plt.title("Significance plot for event FFN binary classifier, using xs_weight")
+plt.xlabel("Discrimintor threshold value")
+plt.xlim(-0.1, 1)
+plt.ylabel("ZA")
+plt.plot(bin_centres_sig, z_vals, 'o-', label='z1')
+plt.plot(bin_centres_sig, z_vals2, 'o-', label='z2')
+plt.legend(loc='upper right')
