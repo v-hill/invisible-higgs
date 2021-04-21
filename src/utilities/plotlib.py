bad69b961973e9526cbb4752e476edd4e01048b4
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-04-21 09:51:45 +0100

    Refactored plotlib
    Added significance plot. Changed how data is inputted into the plotting functions.

--- src/utilities/plotlib.py
+++ src/utilities/plotlib.py
@@ -13,91 +13,103 @@ from sklearn.metrics import roc_curve, auc
 
 # ---------------------------- Plotting functions -----------------------------
 
-def training_history_plot(history, title, figsize=(6, 4), dpi=200, full_y=False):
+def training_history_plot(history_training_data, history_test_data, params, error_bars=[]):
     """
     Plots the training history of the neural network.
 
     Parameters
     ----------
-    history : tensorflow.python.keras.callbacks.History
-        Model training history
-    title : str
-        Plot title
-    figsize : tuple, optional
-        Size of the matplotlib figure. The default is (6, 4).
-    dpi : int, optional
-        The default is 200.
-    full_y : bool, optional
-        Set to true in order to set the y-axis limits to (0, 1). 
-        The default is False, giving automatic scaling.
+    model_results : pandas.DataFrame
+        Dataframe containing the training history data to plot.
+    params : dict
+        Plot parameters.
 
     Returns
     -------
     fig : matplotlib.figure.Figure
     """
-    fig = plt.figure(figsize=figsize, dpi=dpi)
+    fig, ax = plt.subplots(figsize=params['figsize'], dpi=params['dpi'])
+    epochs = np.arange(len(history_training_data))+1
     
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    if full_y:
+    if len(error_bars)==0:
+        ax.plot(epochs, history_training_data, '-x', 
+                lw=1, color=params['colors'][0])
+        ax.plot(epochs, history_test_data, '-x', 
+                lw=1, color=params['colors'][1])
+        ax.annotate(f'max:\n{history_training_data[-1]:0.3f}', 
+                    (epochs[-1], history_training_data[-1]), 
+                    xytext=(0, -15), textcoords='offset points',
+                    ha='center', va='center', 
+                    fontsize=8, color=params['colors'][0])
+    else:
+        ax.errorbar(epochs, history_training_data, 
+                     yerr=error_bars[0], 
+                     capsize=3, lw=1, ls='--' , 
+                     color=params['colors'][0])
+        ax.errorbar(epochs, history_test_data, 
+                     yerr=error_bars[1], 
+                     capsize=3, lw=1, ls='--' ,
+                     color=params['colors'][1])
+        ax.annotate(f'max accuracy:\n{history_training_data[-1]:0.4f} \u00B1 {error_bars[0][-1]:0.4f}', 
+                    (epochs[-1], history_training_data[-1]), 
+                    xytext=(10, -35), textcoords='offset points',
+                    ha='right', va='center', 
+                    fontsize=8, color=params['colors'][0])
+
+
+    if params['full_y']:
         plt.ylim(0, 1)
     
-    plt.title(title)
-    plt.ylabel('Accuracy')
-    plt.xlabel('Epoch number')
-    plt.legend(['train', 'test'], loc='upper left')
+    plt.title(params['title'])
+    plt.xlabel(params['x_axis'])
+    plt.ylabel(params['y_axis'])
+    plt.legend(params['legend'], loc='lower right')
     return fig
 
-def confusion_matrix(cm, class_names, title, figsize=(4, 4), dpi=200, colourbar=False):
+def confusion_matrix(model_results, params):
     """
-    Plots the confusion matrix.
+    Plots the confusion matrix of the neural network evaluated on the test 
+    data.
 
     Parameters
     ----------
-    cm : numpy.ndarray
-        The input confusion matrix
-    class_names : list
-        List of class names 
-    title : str
-        Plot title
-    figsize : tuple, optional
-        Size of the matplotlib figure. The default is (6, 4).
-    dpi : int, optional
-        The default is 200.
-    colourbar : bool, optional
-        Option to add colourbar to plot. The default is False.
+    model_results : pandas.DataFrame
+        Dataframe containing the confusion matrix data to plot.
+    params : dict
+        Plot parameters.
 
     Returns
     -------
     fig : matplotlib.figure.Figure
     """
-    
     # Setup plot
-    fig = plt.figure(figsize=figsize, dpi=dpi)
+    fig = plt.figure(figsize=params['figsize'], dpi=params['dpi'])
     
-    plt.title("\n".join(wrap(title, 30)))
-    if colourbar:
-        plt.colorbar()
-    tick_marks = np.arange(len(class_names))
-    plt.xticks(tick_marks, class_names, rotation=45)
-    plt.yticks(tick_marks, class_names)
+    plt.title("\n".join(wrap(params['title'], 36)))
+    plt.xlabel(params['x_axis'])
+    plt.ylabel(params['y_axis'])
+
+    tick_marks = np.arange(len(params['class_names']))
+    plt.xticks(tick_marks, params['class_names'], rotation=45)
+    plt.yticks(tick_marks, params['class_names'])
     
     # Normalize the confusion matrix
+    cm = model_results['confusion_matrix']
     cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
     
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         plt.text(j, i, cm[i, j], horizontalalignment="center", color="k")
     
     plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Reds)
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    if params['colourbar']:
+        plt.colorbar()
     return fig
 
 def mulit_confusion_matrix(cm_list, class_names, network_names, figsize=(4, 4), dpi=200, norm=True):
-    '''
+    """
     This function returns multiple confusion matrices ploted on the same grid
     to make comparison between networks easier.
-
+    
     Parameters
     ----------
     cm_list : list or array
@@ -111,15 +123,14 @@ def mulit_confusion_matrix(cm_list, class_names, network_names, figsize=(4, 4), 
     title : str
         title 
     figsize : TYPE, optional
-        Figsize. The default is (4, 4).
+        figsize. The default is (4, 4).
     dpi : TYPE, optional
         The default is 200.
-
+        
     Returns
     -------
-    fig.
-
-    '''
+    fig : matplotlib.figure.Figure
+    """
     fig, axes = plt.subplots(ncols=len(cm_list), sharex=True, sharey=True)
     axes[0].set_ylabel('True label')
     tickmarks = np.arange(len(class_names))
@@ -142,39 +153,35 @@ def mulit_confusion_matrix(cm_list, class_names, network_names, figsize=(4, 4), 
             ax.text(j, i, cm_list[idx][i, j], horizontalalignment="center", color="k")
             
     return fig
-    
-def plot_roc(pred, y, title, figsize=(6, 4), dpi=200):
+
+def plot_roc(model_results, params):
     """
-    Plots the ROC curve of a keras model.
-    
+    Plots the ROC curve of a trained keras model.
+
     Parameters
     ----------
-    pred : numpy.ndarray
-        Predicted labels
-    y : numpy.ndarray
-        True labels
-    figsize : tuple, optional
-        DESCRIPTION. The default is (6, 4).
-    dpi : int, optional
-        DESCRIPTION. The default is 200.
+    model_results : pandas.DataFrame
+        Dataframe containing the ROC curve results to plot.
+    params : dict
+        Plot parameters.
 
     Returns
     -------
     fig : matplotlib.figure.Figure
     """
-    fpr, tpr, _ = roc_curve(y, pred)
-    roc_auc = auc(fpr, tpr)
-    fig = plt.figure(figsize=figsize, dpi=dpi)
-    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    fig = plt.figure(figsize=params['figsize'], dpi=params['dpi'])
+    
+    plt.plot(model_results['roc_fpr_vals'],
+             model_results['roc_tpr_vals'], 
+             label=f'ROC curve (area = {model_results["roc_auc"]:0.3f})')
     plt.plot([0, 1], [0, 1], 'k--')
     
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    
-    plt.title(title)
-    plt.legend(loc="lower right")
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
+    plt.xlim([0.0, 1.005])
+    plt.ylim([0.0, 1.005])
+    plt.title(params['title'])
+    plt.xlabel(params['x_axis'])
+    plt.ylabel(params['y_axis'])
+    plt.legend(loc='lower right')
     return fig
 
 def plot_multi_class_roc(pred, y, title, class_labels, figsize=(6, 4), dpi=200):
@@ -339,7 +346,7 @@ def plot_multi_signal_roc(pred, y, title, class_labels, figsize=(6, 4), dpi=200)
     ax.set_ylabel('True Positive Rate')
     return fig
 
-def plot_discriminator_vals(pred_signal, pred_background, title, figsize=(6, 4), dpi=200):
+def plot_discriminator_vals(pred_signal, pred_background, params):
     """
     Plots the distribution of discriminator values for a keras model.
 
@@ -349,29 +356,52 @@ def plot_discriminator_vals(pred_signal, pred_background, title, figsize=(6, 4),
         Array of discriminator values for known signal data
     pred_background : numpy.ndarray
         Array of discriminator values for known background data
-    title : str
-        Plot title
-    figsize : tuple, optional
-        DESCRIPTION. The default is (6, 4).
-    dpi : int, optional
-        DESCRIPTION. The default is 200.
+    params : dict
+        Plot parameters.
 
     Returns
     -------
     fig : matplotlib.figure.Figure
     """
-    
-    bins = np.linspace(0, 1, 50)
 
-    fig = plt.figure(figsize=figsize, dpi=dpi)
-    plt.title(title)
-    plt.xlabel("Label prediction")
-    plt.ylabel("Number of events")
-    # plt.xlim(0, 10)
+    fig = plt.figure(figsize=params['figsize'], dpi=params['dpi'])
     
-    plt.hist(pred_signal, bins, alpha=0.5, label='ttH (signal)', color='brown')
-    plt.hist(pred_background, bins, alpha=0.5, label='tt¯ (background)', color='teal')
+    bins = np.linspace(0, 1, params['num_bins'])
     
+    plt.hist(pred_signal, bins, alpha=0.5, 
+             label='ttH (signal)', color=params['colors'][0])
+    plt.hist(pred_background, bins, alpha=0.5, 
+             label='tt¯ (background)', color=params['colors'][1])
+    
+    plt.title("\n".join(wrap(params['title'], 50)))
+    plt.xlabel(params['x_axis'])
+    plt.ylabel(params['y_axis'])
     plt.legend(loc='upper right')
     return fig
+
+def significance(bin_centres_sig, sig_vals, params):
+    """
+    Significance plot.
+
+    Parameters
+    ----------
+    bin_centres_sig : list
+        List of discriminator values.
+    sig_vals : numpy.ndarray
+        Significance of each discriminator threshold value.
+    params : dict
+        Plot parameters.
+
+    Returns
+    -------
+    fig : matplotlib.figure.Figure
+    """
+    # Setup plot
+    fig = plt.figure(figsize=params['figsize'], dpi=params['dpi'])
     
+    plt.plot(bin_centres_sig, sig_vals)
+    plt.xlim(-0.1, 1)
+    plt.title("\n".join(wrap(params['title'], 60)))
+    plt.xlabel(params['x_axis'])
+    plt.ylabel(params['y_axis'])
+    return fig
