b2dea2db0cf4f928c41f3adfd82aa3edc21b93a4
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-04-24 17:15:51 +0100

    Update binary_event_nn_tuning.py

--- src/hyperparamter_tuning/binary_event_nn_tuning.py
+++ src/hyperparamter_tuning/binary_event_nn_tuning.py
@@ -7,8 +7,9 @@ parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
  
 # Code from other files in the repo
-from utilities.data_analysis import ModelResults, ModelResultsMulti
+import binary_classifier as bcn
 from binary_classifier import EventNN
+from utilities.data_analysis import ModelResultsMulti
 
 # Python libraries
 import numpy as np
@@ -17,11 +18,10 @@ import matplotlib.pyplot as plt
 
 # ------------------------------------ Main -----------------------------------
 
-SAVE_FOLDER = 'data_binary_classifier'
-DIR = SAVE_FOLDER + '\\'
-
+# Full data path directory
+DIR = ('C:..........\\src\\data_binary_classifier\\')
 args_model = {'model_type' : 'binary_classifier',
-              'model_architecture' : 'FNN',
+              'model_architecture' : 'EventNN',
               'layer_1_neurons' : 64,
               'layer_2_neurons' : 8,
               'learning_rate' : 0.001,
@@ -29,22 +29,15 @@ args_model = {'model_type' : 'binary_classifier',
               'epochs' : 8,
               'model' : 'base2'}
 
-num_runs = 10
+num_runs = 5
 dataset_sample = 0.5
 
 test_param = 'layer_1_neurons'
-param_values = np.linspace(4, 16, num=7, dtype=np.int)
+param_values = np.linspace(4, 64, num=7, dtype=np.int)
 
 all_results = ModelResultsMulti()
 event_nn = EventNN(args_model)
-
-
-#%%
-
 event_nn.load_data(DIR)
-
-#%%
-
 event_nn.load_event_data(DIR)
 
 for parameter in param_values:
@@ -53,20 +46,7 @@ for parameter in param_values:
     event_nn.args_model = args_model
 
     for i in range(num_runs):
-        model_result = ModelResults(i)
-        model_result.start_timer()
-    
-        event_nn.shuffle_data()
-        event_nn.reduce_dataset(dataset_sample)
-        event_nn.train_test_split(test_size=0.2)
-        event_nn.create_model(args_model['model'])
-        history = event_nn.train_model(verbose_level=0)
-        
-        # Calculate results
-        model_result.training_history(history)
-        model_result.confusion_matrix(event_nn, cutoff_threshold=0.5)
-        model_result.roc_curve(event_nn)
-        model_result.stop_timer(verbose=True)
+        model_result = bcn.run(i, event_nn, args_model, dataset_sample)
         all_results.add_result(model_result, {test_param : parameter})
 
 df_all_results = all_results.return_results()
@@ -79,11 +59,10 @@ for parameter in param_values:
     test = df_all_results[df_all_results[test_param]==parameter]
     all_data.append(test['accuracy_test'].values)
     
-
 fig, ax = plt.subplots(figsize=(6,4), dpi=200)
-bplot = ax.boxplot(all_data, labels=param_values, showfliers=False, showmeans=True)
+bplot = ax.boxplot(all_data, labels=param_values, 
+                   showfliers=False, showmeans=True)
 
 ax.yaxis.grid(True)
 ax.set_xlabel('Number of neurons')
 ax.set_ylabel('Model accuracy')
-
