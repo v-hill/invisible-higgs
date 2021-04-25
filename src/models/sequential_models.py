bfaf25a4c856bc49177ca6bd69306459d282aefc
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-04-25 19:33:24 +0100

    Changed input to args dict

--- src/models/sequential_models.py
+++ src/models/sequential_models.py
@@ -8,7 +8,6 @@ models.
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
 
 USE_GPU = False
 if USE_GPU:
@@ -22,48 +21,21 @@ else:
 
 # -----------------------------------------------------------------------------  
 
-def base(layer1, layer2, input_shape=11):
+def base(args):
     """
     This function creates a dense/fully connected neuarl network with 2 hidden
-    layers.
-    Parameters
-    ----------
-    layer1 : int
-        Number of neurons in the first layer
-    layer2 : int
-        Number of neurons in the second layer
-    input_shape : int, optional
-        The default is 12.
-    Returns
-    -------
-    model : keras.Sequential
-    """
-    # Define Sequential model with 2 hidden layers
-    model = keras.Sequential()
-    model.add(keras.Input(shape=(input_shape,)))
-    model.add(layers.Dense(layer1, activation="relu"))
-    model.add(layers.Dense(layer2, activation="relu"))
-    model.add(layers.Dense(2))
-    
-    # Compile model
-    model.compile(optimizer='adam',
-                  loss=SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-    return model
-
-def base2(layer1, layer2, input_shape, learning_rate=0):
-    """
-    base + custom learning rate for the Adam optimiser
-         + 'binary_crossentropy' loss function
+    layers. Uses custom learning rate for the Adam optimiser. Uses 
+    'binary_crossentropy' loss function.
 
     Parameters
     ----------
-    layer1 : int
-        Number of neurons in the first layer
-    layer2 : int
-        Number of neurons in the second layer
-    input_shape : int, optional
-        The default is 12.
+    args : dict
+        Dictionary of values to use in creating the model.
+        Contains the following values as an example:
+             {'layer_1_neurons' : 64,
+              'layer_2_neurons' : 8,
+              'output_shape' : 1,
+              'learning_rate' : 0.001}
 
     Returns
     -------
@@ -71,46 +43,55 @@ def base2(layer1, layer2, input_shape, learning_rate=0):
     """
     # Define Sequential model with 2 hidden layers
     model = keras.Sequential()
-    model.add(keras.Input(shape=(input_shape,)))
-    model.add(layers.Dense(layer1, activation='relu', 
+    model.add(keras.Input(shape=(args['event_layer_input_shape'],)))
+    model.add(layers.Dense(args['layer_1_neurons'], 
+                           activation='relu', 
                            kernel_initializer='random_normal'))
-    model.add(layers.Dense(layer2, activation='relu', 
+    model.add(layers.Dense(args['layer_2_neurons'], 
+                           activation='relu', 
                            kernel_initializer='random_normal'))
-    model.add(layers.Dense(1, activation='sigmoid', 
+    model.add(layers.Dense(args['output_shape'], 
+                           activation='sigmoid', 
                            kernel_initializer='random_normal'))
-    
     # Compile model
-    if learning_rate == 0:
+    if args['learning_rate'] == 0:
         opt = keras.optimizers.Adam()
     else:
-        opt = keras.optimizers.Adam(learning_rate=learning_rate)
+        opt = keras.optimizers.Adam(learning_rate=args['learning_rate'])
     model.compile(optimizer=opt,
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
     return model
 
-def base_with_dropout(layer1, layer2, input_shape=11, dropout=0.2):
+def base_with_dropout(args, dropout=0.2):
     """
-    Functionally identical to base3 model, but with the addition of two 
+    Functionally identical to base model, but with the addition of two 
     dropout layers after each hidden layer.
+    
     Returns
     -------
     model : keras.Sequential
     """
     # Define Sequential model with 2 hidden layers
     model = keras.Sequential()
-    model.add(keras.Input(shape=(input_shape,)))
-    model.add(layers.Dense(layer1, activation='relu', 
+    model.add(keras.Input(shape=(args['event_layer_input_shape'],)))
+    model.add(layers.Dense(args['layer_1_neurons'], 
+                           activation='relu', 
                            kernel_initializer='random_normal'))
     model.add(layers.Dropout(dropout))
-    model.add(layers.Dense(layer2, activation='relu', 
+    model.add(layers.Dense(args['layer_2_neurons'], 
+                           activation='relu', 
                            kernel_initializer='random_normal'))
     model.add(layers.Dropout(dropout))
-    model.add(layers.Dense(1, activation='sigmoid', 
+    model.add(layers.Dense(args['output_shape'], 
+                           activation='sigmoid', 
                            kernel_initializer='random_normal'))
-    
     # Compile model
-    model.compile(optimizer='adam',
+    if args['learning_rate'] == 0:
+        opt = keras.optimizers.Adam()
+    else:
+        opt = keras.optimizers.Adam(learning_rate=args['learning_rate'])
+    model.compile(optimizer=opt,
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
     return model
@@ -145,5 +126,3 @@ def multi_class_base(layer1, layer2, input_shape=11, output_shape=4):
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
     return model
-
-models_list = [base, base2, base_with_dropout]
