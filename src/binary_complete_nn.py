48313655add5f0be1c9dd3307c84e79219388a74
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-04-26 08:56:04 +0100

    Updated from binary_classifier to classifier
    Put main function in an ' if __name__ == "__main__": ' if statement. 
    Added additional model arguments.

--- src/binary_complete_nn.py
+++ src/binary_complete_nn.py
@@ -7,31 +7,40 @@ network (for the jet data).
 # ---------------------------------- Imports ----------------------------------
 
 # Code from other files in the repo
-import binary_classifier as bcn
-from binary_classifier import CombinedNN
+import classifier
 import utilities.plotlib as plotlib
 from utilities.data_analysis import ModelResultsMulti
 
 # ------------------------------------ Main -----------------------------------
 
-DIR = 'data_binary_classifier\\'
-args_model = {'model_type' : 'binary_classifier',
-              'model_architecture' : 'CombinedNN',
-              'batch_size' : 64,
-              'epochs' : 8,
-              'model' : 'base1'}
-
-num_runs = 1
-dataset_sample = 0.25
-
-all_results = ModelResultsMulti()
-neural_net = CombinedNN(args_model)  
-neural_net.load_data(DIR)
-neural_net.load_event_data(DIR)
-neural_net.load_jet_data(DIR)
-
-for i in range(num_runs):
-    model_result = bcn.run(i, neural_net, args_model, dataset_sample)
-    all_results.add_result(model_result, args_model)
-
-df_all_results = all_results.return_results()
+if __name__ == "__main__":
+    DIR = 'data_binary_classifier\\'
+    
+    args_model = {'model_type' : 'binary_classifier',
+                  'model_architecture' : 'CombinedNN',
+                  'ffn_layer_1_neurons' : 16,
+                  'ffn_layer_2_neurons' : 8,
+                  'rnn_layer_1_neurons' : 64,
+                  'rnn_layer_2_neurons' : 8,
+                  'final_layer_neurons' : 8,
+                  'output_shape' : 1,
+                  'learning_rate' : 0,
+                  'batch_size' : 64,
+                  'epochs' : 16,
+                  'model' : 'base'}
+    
+    num_runs = 5
+    dataset_sample = 0.25
+    
+    all_results = ModelResultsMulti()
+    neural_net = classifier.CombinedNN(args_model)  
+    neural_net.load_data(DIR)
+    neural_net.load_event_data(DIR)
+    neural_net.load_jet_data(DIR)
+    
+    for i in range(num_runs):
+        model_result = classifier.run(i, neural_net, args_model, dataset_sample)
+        all_results.add_result(model_result, args_model)
+    
+    df_all_results = all_results.return_results()
+    all_results.save('binary_commbined_nn.pkl')
