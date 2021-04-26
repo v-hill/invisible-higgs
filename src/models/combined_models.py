1c9bf25f6fdfcf46f5cdaf7496520974e0b0ff5a
V. Hill <67915924+V-Hill@users.noreply.github.com>
2021-04-26 09:00:25 +0100

    Switched to args dict input for base functions

--- src/models/combined_models.py
+++ src/models/combined_models.py
@@ -21,7 +21,7 @@ from tensorflow.keras import layers
 
 # ----------------------------------- Models ---------------------------------- 
 
-def base1(input_shape1, input_shape2):
+def base(args):
     """
     Create a multi-input combined neural network with two branches.
     One branch uses an FFN for the event data, the other branch and RNN on the
@@ -29,28 +29,39 @@ def base1(input_shape1, input_shape2):
 
     Parameters
     ----------
-    input_shape1 : int
-        Shape of event level variable data.
-    input_shape2 : list
-        Dimensions of ragged tensor jet data.
-
+    args : dict
+        Dictionary of values to use in creating the model. e.g.
+        {'ffn_layer_1_neurons' : 16,
+        'ffn_layer_2_neurons' : 8,
+        'rnn_layer_1_neurons' : 64,
+        'rnn_layer_2_neurons' : 8,
+        'final_layer_neurons' : 8,
+        'output_shape' : 1,
+        'learning_rate' : 0.001}
+        
     Returns
     -------
     model : tensorflow.python.keras.engine.functional.Functional
         The complete neural network
     """
     # define two seperate inputs
-    inputA = keras.Input(input_shape1)
-    inputB = keras.Input(input_shape2, ragged=True)
+    inputA = keras.Input(args['event_layer_input_shape'])
+    inputB = keras.Input(args['jet_layer_input_shape'], ragged=True)
     
     # create the sequential event nn
-    x = layers.Dense(42, activation="relu")(inputA)
-    x = layers.Dense(4, activation="relu")(x)
+    x = layers.Dense(args['ffn_layer_1_neurons'], 
+                     activation="relu", 
+                     kernel_initializer='random_normal')(inputA)
+    x = layers.Dense(args['ffn_layer_2_neurons'], 
+                     activation="relu", 
+                     kernel_initializer='random_normal')(x)
     x = keras.Model(inputs=inputA, outputs=x)
     
     # the second branch opreates on the second input
-    y = keras.layers.LSTM(64)(inputB)
-    y = keras.layers.Dense(4, activation="relu")(y)
+    y = keras.layers.LSTM(args['rnn_layer_1_neurons'])(inputB)
+    y = keras.layers.Dense(args['rnn_layer_2_neurons'], 
+                           activation="relu", 
+                           kernel_initializer='random_normal')(y)
     y = keras.Model(inputs=inputB, outputs=y)
     
     # combine the output of the two branches
@@ -58,11 +69,21 @@ def base1(input_shape1, input_shape2):
     
     # apply a FC layer and then a regression prediction on the
     # combined outputs
-    z = layers.Dense(4, activation="relu")(combined)
-    z = layers.Dense(1, activation='sigmoid')(z)
+    z = layers.Dense(args['final_layer_neurons'], 
+                     activation="relu", 
+                     kernel_initializer='random_normal')(combined)
+    z = layers.Dense(args['output_shape'], activation='sigmoid')(z)
     
     model = keras.Model(inputs=[x.input, y.input], outputs=z)
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    
+    # Compile model
+    if args['learning_rate'] == 0:
+        opt = keras.optimizers.Adam()
+    else:
+        opt = keras.optimizers.Adam(learning_rate=args['learning_rate'])
+    model.compile(optimizer=opt,
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
     return model
 
 def multi_label_base(input_shape1, input_shape2, output_shape=4):
@@ -88,13 +109,13 @@ def multi_label_base(input_shape1, input_shape2, output_shape=4):
     inputB = keras.Input(input_shape2, ragged=True)
     
     # create the sequential event nn
-    x = layers.Dense(64, activation="relu")(inputA)
-    x = layers.Dense(8, activation="relu")(x)
+    x = layers.Dense(42, activation="relu")(inputA)
+    x = layers.Dense(4, activation="relu")(x)
     x = keras.Model(inputs=inputA, outputs=x)
     
     # the second branch opreates on the second input
-    y = keras.layers.LSTM(64)(inputB)
-    y = keras.layers.Dense(8, activation="relu")(y)
+    y = keras.layers.LSTM(16)(inputB)
+    y = keras.layers.Dense(16, activation="relu")(y)
     y = keras.Model(inputs=inputB, outputs=y)
     
     # combine the output of the two branches
